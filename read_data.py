42863368a9d54a819c74d48bd469983e37402662
XuYining <oxuyining@gmail.com>
2019-08-14 15:42:18 +0800

    vae-pre

---
+++ read_data.py
@@ -0,0 +1,82 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import random
+
+# mnist = input_data.read_data_sets("./dataset/mnist/")
+# specific_idx = np.where(mnist.train.labels == 1)[0]
+# data = mnist.train.images[specific_idx].reshape(-1, 28, 28, 1)
+# print(np.max(data))
+# c_dim = 1
+
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def read_data(labels):
+    dict_train = unpickle('dataset/cifar-10/data_batch_1')
+    train_data = dict_train[b'data']
+    train_labels = dict_train[b'labels']
+    dict_train = unpickle('dataset/cifar-10/data_batch_2')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('dataset/cifar-10/data_batch_3')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('dataset/cifar-10/data_batch_4')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    dict_train = unpickle('dataset/cifar-10/data_batch_5')
+    train_data = np.append(train_data, dict_train[b'data'], axis=0)
+    train_labels.extend(dict_train[b'labels'])
+    # dict_test = unpickle('dataset/cifar-10/test_batch')
+    # test_data = dict_test[b'data']
+    # test_labels = dict_test[b'labels']
+    train_labels = np.array(train_labels)
+    specific_idx = np.where(train_labels == labels)
+    train_data = train_data[specific_idx]
+    train_data = np.reshape(train_data, [-1, 32, 32, 3], 'F')
+    # train_data = np.dot(train_data[..., :3], [0.299, 0.587, 0.114])
+    # train_data = np.reshape(train_data, [-1, 32, 32, 1])
+    train_data = train_data / 255.
+    train_data = np.transpose(train_data, [0, 2, 1, 3])
+    # train_data = (train_data - 128) /128.0
+    return train_data
+
+
+def test_data(labels):
+    dict_test = unpickle('dataset/cifar-10/test_batch')
+    test_data = dict_test[b'data']
+    test_labels = dict_test[b'labels']
+    train_labels = np.array(test_labels)
+    specific_idx = np.where(train_labels == labels)[0]
+    specific_idx_anomaly = np.where(train_labels != labels)[0]
+    ten_precent_anomaly = [specific_idx_anomaly[x] for x in
+                                   random.sample(range(0, len(specific_idx_anomaly)), len(specific_idx_anomaly) // 9)]
+    ten_precent_anomaly = np.array(ten_precent_anomaly)
+
+    test_data1 = test_data[specific_idx]
+    test_labels = np.array(test_labels)
+    test_label1 = test_labels[specific_idx]
+    tmp_data = test_data[ten_precent_anomaly]
+    test_data = np.append(test_data1, tmp_data, axis=0)
+    test_label2 = test_labels[ten_precent_anomaly]
+    test_label = np.append(test_label1, test_label2, axis=0)
+    test_data = np.reshape(test_data, [-1, 32, 32, 3], 'F')
+    test_data = np.transpose(test_data, [0, 2, 1, 3])
+    # plt.imshow(test_data[0])
+    # plt.show()
+    test_data = test_data / 128.0 - 1
+    test_label.tolist()
+    test_label = list(test_label)
+    for i in range(len(test_label)):
+        if test_label[i] == labels:
+            test_label[i] = 1
+        else:
+            test_label[i] = 0
+
+    return test_data, test_label
+
+# cifar = test_data(1)
+# a = 1
