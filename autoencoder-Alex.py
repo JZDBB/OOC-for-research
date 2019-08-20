3fa6efc19081fd1df42132d16aa36146446fe760
XuYining <oxuyining@gmail.com>
2019-08-20 18:17:57 +0800

    cifar-10 autoencoder pytorch-version need debug

--- autoencoder-Alex.py
+++ autoencoder-Alex.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, CIFAR10
 import os
 
 if not os.path.exists('./dc_img'):
@@ -24,19 +24,19 @@ batch_size = 128
 learning_rate = 1e-3
 
 # RGB
-# img_transform = transforms.Compose([
-#     transforms.ToTensor(),
-#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-# ])
-
-# gray image
 img_transform = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-    ])
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+# gray image
+# img_transform = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.1307,), (0.3081,))
+#     ])
 
 
-dataset = MNIST('./data', transform=img_transform, download=True)
+dataset = CIFAR10('./data', transform=img_transform, download=True)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 
@@ -44,24 +44,32 @@ class autoencoder(nn.Module):
     def __init__(self):
         super(autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1),  # b, 16, 14, 14
+            nn.Conv2d(3, 64, 7, stride=2, padding=2),  # b, 16, 14, 14
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),  # b, 16, 13, 13
-            nn.Conv2d(16, 32, 3, stride=2, padding=1),  # b, 32, 7, 7
+            nn.MaxPool2d(3, stride=2),  # b, 16, 13, 13
+            nn.Conv2d(64, 256, 5, stride=1, padding=2),  # b, 32, 7, 7
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=1),  # b, 32, 6, 6
-            nn.Conv2d(32, 64, 3, stride=2, padding=1),  # b, 64, 3, 3
+            nn.MaxPool2d(3, stride=2),  # b, 32, 6, 6
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),  # b, 64, 3, 3
             nn.ReLU(True),
-            nn.MaxPool2d(2, stride=2, padding=1)  # b, 64, 2, 2
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),  # b, 64, 3, 3
+            nn.ReLU(True),
+            nn.Conv2d(512, 256, 3, stride=1, padding=1),  # b, 64, 3, 3
+            nn.ReLU(True),
+            # b, 256, 3, 3
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1),  # b, 32, 3, 3
+            nn.ConvTranspose2d(256, 512, 3, stride=1, padding=1),  # b, 512, 3, 3
             nn.ReLU(True),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1),  # b, 16, 5, 5
+            nn.ConvTranspose2d(512, 512, 3, stride=2, padding=1),  # b, 16, 5, 5
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ConvTranspose2d(512, 256, 5, stride=3, padding=1),  # b, 8, 15, 15
             nn.ReLU(True),
-            nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+            nn.ConvTranspose2d(256, 64, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 16, 5, stride=3, padding=1),  # b, 8, 15, 15
+            nn.ReLU(True),
+            nn.ConvTranspose2d(16, 3, 2, stride=2, padding=1),  # b, 1, 28, 28
             nn.Tanh()
         )
 
